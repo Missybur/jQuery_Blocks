7fff7499dfa7aba33a40cfa4f657c3dcc29c11fd
Missy Bur <missybur@gmail.com>
2015-08-27 15:49:59 -0700

    Changes made to jquery challenge

--- main.js
+++ main.js
@@ -1,1 +1,14 @@
-My Page
+$(document).ready(init);
+function init () {
+  $('.box').on({
+    mouseover: function() {
+      $(this).css({background: '#FF0000'});
+    },
+    mouseleave: function(){
+      $(this).css({background: '#000000'})
+    },
+    click: function(){
+      $(this).off('mouseleave');
+    }
+  });
+}
